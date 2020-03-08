c5c0c5f972cc6153201d29451081c29832facb92
UnlimitedHugs <unlimited.hugs@gmail.com>
2020-03-08 18:34:42 +0400

    Improved squad interest point selection

--- Source/PawnSquadSelector.cs
+++ Source/PawnSquadSelector.cs
@@ -2,67 +2,88 @@
 using System.Linq;
 using HugsLib.Utils;
 using RimWorld.Planet;
+using UnityEngine;
 using Verse;
 
 namespace DefensivePositions {
 	/// <summary>
-	/// Handles selecting and focusing squad members when a squad is activated
+	/// Handles selecting and focusing squad members when a squad is activated.
+	/// Repeated activations of the same squad will cycle through all "interest points" of this squad-
+	/// including clusters of pawns on each map, as well as caravans on the world map
 	/// </summary>
 	public class PawnSquadSelector {
-		private PawnSquad lastActivatedSquad;
-		private int repeatedSquadActivations;
+		// how long it takes for an additional press of the squad hotkey to count as the first in a sequence
+		private const float ActivationSequenceExpireTime = 3f;
+
+		private int lastActivatedSquadId;
+		private float lastActivationTime;
+		/* 
+		Stores the map index where cycling began. Null if starting on the world map.
+		Must be stored to preserve order when jumping between interest points.
+		 */
+		private int? lastStartingMapIndex;
+		/* 
+		Allows to sort interest points by increasing distance from camera. Stored to preserve order when jumping between interest points.
+		This position won't carry any meaning when jumping to other maps, but we don't care about minimizing jump distance on those, either. 
+		*/
+		private IntVec3 lastStartingMapCameraPosition;
+		private int interestPointIndex;
+
+		private static Map CurrentlyViewedMap {
+			get { return PawnSquadHandler.ViewingWorldMap ? null : Find.CurrentMap; }
+		}
 
 		public bool TryActivateSquad(PawnSquad squad) {
-			if(!HugsLibUtility.ShiftIsHeld) Find.Selector.ClearSelection();
 			var members = squad?.ValidMembers.ToArray();
 			if (members != null && members.Length > 0) {
-				ProcessRepeatedActivation(squad);
-				var potentialFocusTargets = EnumerateCameraFocusTargets(members).ToArray();
-				// all squad members may have been despawned, and not found in any caravan. If so, report squad as unassigned
-				if (potentialFocusTargets.Length > 0) {
+				var isFirstActivationInSequence = CheckForFirstActivation(squad);
+				var potentialInterestPoints = EnumerateInterestPoints(members, lastStartingMapCameraPosition).ToList();
+				SortInterestPointsByStartingMap(potentialInterestPoints, lastStartingMapIndex);
+				// if all squad members have been despawned, and not found in any caravan, report squad as unassigned
+				if (potentialInterestPoints.Count > 0) {
 					// loop back to fist target after reaching the end
-					var currentActivationTarget = potentialFocusTargets[repeatedSquadActivations % potentialFocusTargets.Length];
-					CameraJumper.TryJump(currentActivationTarget);
-					if (currentActivationTarget.HasWorldObject) {
-						// current target is a caravan
-						Find.WorldSelector.ClearSelection();
-						Find.WorldSelector.Select(currentActivationTarget.WorldObject);
-					} else {
-						// current target must be a map location
-						var currentMap = currentActivationTarget.Map;
-						if (currentMap != null) {
-							var squadMembersOnCurrentMap = members.Where(t => t.Map == currentMap);
-							foreach (var thing in squadMembersOnCurrentMap) {
-								Find.Selector.Select(thing);
-							}
-						}
+					var currentActivationTarget = potentialInterestPoints[interestPointIndex % potentialInterestPoints.Count];
+					var shouldJumpToTarget = currentActivationTarget.IsWorldTarget
+											|| !isFirstActivationInSequence
+											|| CurrentlyViewedMap != currentActivationTarget.Map;
+					if (shouldJumpToTarget) {
+						CameraJumper.TryJump(currentActivationTarget);
+						interestPointIndex++;
 					}
+					TrySelectActivationTargetSquadMembers(members, currentActivationTarget);
 					return true;
 				}
 			}
 			return false;
 		}
 
-		private void ProcessRepeatedActivation(PawnSquad squad) {
-			var isRepeatedActivation = squad == lastActivatedSquad;
-			if (isRepeatedActivation) {
-				repeatedSquadActivations++;
+		private bool CheckForFirstActivation(PawnSquad activatedSquad) {
+			bool isFirstActivation;
+			if (lastActivatedSquadId != activatedSquad.SquadId || Time.unscaledTime > lastActivationTime + ActivationSequenceExpireTime) {
+				lastActivatedSquadId = activatedSquad.SquadId;
+				interestPointIndex = 0;
+				lastStartingMapIndex = CurrentlyViewedMap?.Index;
+				lastStartingMapCameraPosition = Find.CurrentMap.rememberedCameraPos.rootPos.ToIntVec3();
+				isFirstActivation = true;
 			} else {
-				repeatedSquadActivations = 0;
+				isFirstActivation = false;
 			}
-			lastActivatedSquad = squad;
+			lastActivationTime = Time.unscaledTime;
+			return isFirstActivation;
 		}
 
-		private IEnumerable<GlobalTargetInfo> EnumerateCameraFocusTargets(IEnumerable<Thing> things) {
+		private static IEnumerable<GlobalTargetInfo> EnumerateInterestPoints(IEnumerable<Thing> things, IntVec3 startingPosition) {
 			var sameClusterDistance = DefensivePositionsManager.Instance.SameGroupDistanceSetting.Value;
-			var caravans = EnumerateCaravansWithPawns(things.OfType<Pawn>()).ToArray();
+			var caravans = EnumerateCaravansWithPawns(things).ToArray();
 			var spawnedThings = things.Where(t => t != null && t.Spawned); // spawned things are not part of caravans
 			var spawnedThingsGroupedByMap = spawnedThings.GroupBy(t => t.Map);
 			// return centers of clustered pawns first
 			foreach (var group in spawnedThingsGroupedByMap) {
-				var mapGroupThingPositions = group.Select(t => t.Position);
-				foreach (var clusterCenterPos in EnumerateClusteredPositionCenters(mapGroupThingPositions, sameClusterDistance)) {
-					yield return new GlobalTargetInfo(clusterCenterPos, group.Key);
+				var map = group.Key;
+				var thingPositions = group.Select(t => t.Position);
+				var clusterPositions = EnumerateClusteredPositionCenters(thingPositions, sameClusterDistance, startingPosition);
+				foreach (var clusterCenterPos in clusterPositions) {
+					yield return new GlobalTargetInfo(clusterCenterPos, map);
 				}
 			}
 			// return caravan positions second
@@ -71,16 +92,32 @@ namespace DefensivePositions {
 			}
 		}
 
-		private IEnumerable<IntVec3> EnumerateClusteredPositionCenters(IEnumerable<IntVec3> positions, float maxClusterMemberDistance) {
-			// for simplicity, the first encountered position is used as base when testing other positions for membership in the same cluster
+		/// <summary>
+		/// Groups given positions into clusters by distance from each other
+		/// </summary>
+		/// <param name="positions">Positions to sort into clusters</param>
+		/// <param name="maxClusterMemberDistance">Maximum distance between positions to be considered part of the same cluster</param>
+		/// <param name="startingPosition">Clusters are returned in order of increasing distance from starting position or previous cluster</param>
+		/// <returns>Average of all positions part of each cluster, rounded to the nearest cell</returns>
+		private static IEnumerable<IntVec3> EnumerateClusteredPositionCenters(
+			IEnumerable<IntVec3> positions, float maxClusterMemberDistance, IntVec3 startingPosition) {
 			var positionsPool = positions.ToList();
 			var currentCluster = new List<IntVec3>();
-			void PrimeCluster() {
-				// try to preserve ordering by always starting with the first pool element
-				currentCluster.Add(positionsPool[0]);
-				positionsPool.RemoveAt(0);
+			var currentClusterBasePosition = startingPosition;
+			IntVec3 TakeClosestPositionFromPool(IntVec3 basePos) {
+				var closestIndex = GetIndexOfClosestPosition(basePos, positionsPool);
+				var closestPos = positionsPool[closestIndex];
+				positionsPool.RemoveAt(closestIndex);
+				return closestPos;
 			}
-			bool PositionIsWithinCurrentCluster(IntVec3 pos) => currentCluster[0].InHorDistOf(pos, maxClusterMemberDistance);
+			void PrimeCluster() {
+				var clusterBasePosition = TakeClosestPositionFromPool(currentClusterBasePosition);
+				currentCluster.Add(clusterBasePosition);
+				currentClusterBasePosition = clusterBasePosition;
+			}
+			bool PositionIsWithinCurrentCluster(IntVec3 pos) {
+				return currentClusterBasePosition.InHorDistOf(pos, maxClusterMemberDistance);
+			}
 			while (positionsPool.Count > 0) {
 				PrimeCluster();
 				for (var i = positionsPool.Count - 1; i >= 0; i--) {
@@ -94,14 +131,26 @@ namespace DefensivePositions {
 			}
 		}
 
-		private IEnumerable<Caravan> EnumerateCaravansWithPawns(IEnumerable<Pawn> pawns) {
+		private static void SortInterestPointsByStartingMap(List<GlobalTargetInfo> interestPoints, int? startingMapIndex) {
+			// targets on the first activation map should be returned first
+			int GetTargetPriority(GlobalTargetInfo t) {
+				if (startingMapIndex.HasValue) {
+					return t.Map != null && t.Map.Index == startingMapIndex ? 1 : 2;
+				} else {
+					return t.HasWorldObject ? 1 : 2;
+				}
+			}
+			interestPoints.SortStable((a, b) => GetTargetPriority(a).CompareTo(GetTargetPriority(b)));
+		}
+
+		private static IEnumerable<Caravan> EnumerateCaravansWithPawns(IEnumerable<Thing> pawns) {
 			var caravans = Find.WorldObjects.Caravans;
-			var searchedPawnSed = pawns.ToHashSet();
+			var searchedPawnSet = pawns.ToHashSet();
 			foreach (var caravan in caravans) {
 				if(!caravan.IsPlayerControlled) continue;
 				var containsAnySearchedPawn = false;
 				foreach (var pawn in caravan.PawnsListForReading) {
-					if (searchedPawnSed.Contains(pawn)) {
+					if (searchedPawnSet.Contains(pawn)) {
 						containsAnySearchedPawn = true;
 						break;
 					}
@@ -112,7 +161,7 @@ namespace DefensivePositions {
 			}
 		}
 
-		private IntVec3 TryGetAveragePosition(List<IntVec3> positions) {
+		private static IntVec3 TryGetAveragePosition(List<IntVec3> positions) {
 			if (positions.Count > 0) {
 				var sum = IntVec3.Zero;
 				for (var i = 0; i < positions.Count; i++) {
@@ -121,6 +170,43 @@ namespace DefensivePositions {
 				return new IntVec3(sum.x / positions.Count, 0, sum.z / positions.Count);
 			}
 			return IntVec3.Invalid;
+		}
+
+		private static int GetIndexOfClosestPosition(IntVec3 basePos, List<IntVec3> positions) {
+			var closestPosIndex = -1;
+			float closestPosDistanceSquared = float.MaxValue;
+			for (var i = 0; i < positions.Count; i++) {
+				var distanceSquared = positions[i].DistanceToSquared(basePos);
+				if (distanceSquared < closestPosDistanceSquared) {
+					closestPosIndex = i;
+					closestPosDistanceSquared = distanceSquared;
+				}
+			}
+			return closestPosIndex;
+		}
+
+		private static IEnumerable<Thing> FilterThingsByMap(IEnumerable<Thing> members, Map currentMap) {
+			return members.Where(t => t.Map == currentMap);
+		}
+
+		private static void TrySelectActivationTargetSquadMembers(IEnumerable<Thing> squadMembers, GlobalTargetInfo activationTarget) {
+			var additiveSelection = HugsLibUtility.ShiftIsHeld;
+			if (activationTarget.HasWorldObject) {
+				// current target is a caravan
+				if (!additiveSelection) Find.WorldSelector.ClearSelection();
+				Find.WorldSelector.ClearSelection();
+				Find.WorldSelector.Select(activationTarget.WorldObject);
+			} else {
+				// current target must be a map location
+				if (!additiveSelection) Find.Selector.ClearSelection();
+				var currentMap = activationTarget.Map;
+				if (currentMap != null) {
+					var squadMembersOnCurrentMap = FilterThingsByMap(squadMembers, currentMap);
+					foreach (var thing in squadMembersOnCurrentMap) {
+						Find.Selector.Select(thing);
+					}
+				}
+			}
 		}
 	}
 }
